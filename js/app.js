8d83ccdbf7a922f450abae130c1e9ce54e947884
Wise-Wife <73318124+mwise0829@users.noreply.github.com>
2021-09-22 07:24:55 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,26 +2,27 @@ const bar = document.querySelector('#navbar__list');
 const menuItem = document.querySelectorAll('section');
 
 // build the nav
-function createNavbar () {
-    const fragment = document.createDocumentFragment();
+function createNavbar() {
+  const fragment = document.createDocumentFragment();
 
-    menuItem.forEach((navItem) => {
-        var li = document.createElement('li');
-        var a = document.createElement('a');
-        a.innerText = navItem.getAttribute('data-nav');
-        a.setAttribute('class', 'menu__link');
+  menuItem.forEach((navItem) => {
+    var li = document.createElement('li');
+    var a = document.createElement('a');
+    a.innerText = navItem.getAttribute('data-nav');
+    a.setAttribute('class', 'menu__link');
 
-        // Scroll to section on link click
-        a.addEventListener("click", () => {
-         navItem.scrollIntoView({behavior: "auto"})
-        });
-          li.appendChild(a);
-          fragment.appendChild(li)
-          ;
-        });
+    // Scroll to section on link click
+    a.addEventListener("click", () => {
+      navItem.scrollIntoView({ behavior: "auto" });
+      navItem.style.backgroundColor = '#00000026';
+      li.style.backgroundColor = '#00000026';
+    });
+    li.appendChild(a);
+    fragment.appendChild(li);
+  });
 
-        bar.appendChild(fragment);
-    };
+  bar.appendChild(fragment);
+};
 
 
 /**
@@ -31,11 +32,4 @@ function createNavbar () {
 */
 
 // Build menu 
-createNavbar ();
-
-
-
-
-// Set sections as active
-
-
+createNavbar();
